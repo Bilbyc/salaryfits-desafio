9e9a89395d09b64a3b7b13857b0e21d4e7dee0b2
Carlos Bilby <carlos.bilby@gmail.com>
2024-09-08 18:34:46 -0300

    feat: ajusta modules de autenticação

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,16 @@ import { AppService } from './app/services/app.service';
 import { ContaController } from './app/infrastructure/controllers/conta/conta.controller';
 import { ServicesModule } from './app/services/services.module';
 import { TransacaoController } from './app/infrastructure/controllers/transacao/transacao.controller';
+import { AuthController } from './app/infrastructure/adapters/auth/controller/auth.controller';
 
 @Module({
   imports: [ServicesModule],
-  controllers: [AppController, ContaController, TransacaoController],
+  controllers: [
+    AppController,
+    ContaController,
+    TransacaoController,
+    AuthController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { RepositoriesModule } from '../infrastructure/repositories/repositories.module';
 import { ContaService } from './conta.service';
 import { TransacaoService } from './transacao.service';
+import { AuthService } from '../infrastructure/adapters/auth/service/auth.service';
 
 @Module({
   imports: [RepositoriesModule],
-  providers: [ContaService, TransacaoService],
-  exports: [ContaService, TransacaoService],
+  providers: [ContaService, TransacaoService, AuthService],
+  exports: [ContaService, TransacaoService, AuthService],
 })
 export class ServicesModule {}
