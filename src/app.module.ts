501914cac3ee2984fa814d6bd176fd93190e0bac
Carlos Bilby <carlos.bilby@gmail.com>
2024-09-08 02:33:48 -0300

    feat: adiciona service para instanciar prisma client

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app/infrastructure/controllers/app.controller';
 import { AppService } from './app/services/app.service';
 import { ContaController } from './app/infrastructure/controllers/conta/conta.controller';
+import { PrismaService } from './app/infrastructure/prisma/prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController, ContaController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
