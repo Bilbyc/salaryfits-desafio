1c3cd34f3c35de2867f6f68101bd3717c1b76293
Carlos Bilby <carlos.bilby@gmail.com>
2024-09-08 03:20:07 -0300

    feat: cria repositories module para gerenciar e exportar todos os repositorios de domain classes

---
+++ src/app/infrastructure/repositories/repositories.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { IContaRepository } from '../../domain/conta/repositories/iconta.repository';
+import { ContaRepository } from './conta.repository';
+
+@Module({
+  providers: [
+    PrismaService,
+    {
+      provide: IContaRepository,
+      useClass: ContaRepository,
+    },
+  ],
+})
+export class RepositoriesModule {}
