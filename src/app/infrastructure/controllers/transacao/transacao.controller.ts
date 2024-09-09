1d8db204a93ac4bd0c44ff347906ad6e9fb11b91
Carlos Bilby <carlos.bilby@gmail.com>
2024-09-09 02:30:14 -0300

    refactor: remove chamada desnecessaria de Guards globais

--- src/app/infrastructure/controllers/transacao/transacao.controller.ts
+++ src/app/infrastructure/controllers/transacao/transacao.controller.ts
@@ -1,24 +1,13 @@
-import {
-  Body,
-  Controller,
-  HttpStatus,
-  Post,
-  Req,
-  Res,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Req, Res } from '@nestjs/common';
 import { TransacaoService } from '../../../services/transacao.service';
 import { Request, Response } from 'express';
 import { CreateTransferenciaDto } from '../../../domain/transacao/dtos/create-transferencia.dto';
-import { AuthGuard } from '../../adapters/authentication/auth.guard';
 import { Role } from '../../../domain/conta/conta';
 import { Roles } from '../../adapters/authorization/role.decorator';
-import { RolesGuard } from '../../adapters/authorization/roles.guard';
 
 @Controller('api/transacao')
 export class TransacaoController {
   constructor(private transacaoService: TransacaoService) {}
-  @UseGuards(AuthGuard, RolesGuard)
   @Post('/transferir')
   @Roles(Role.Admin)
   async transferirSaldo(
