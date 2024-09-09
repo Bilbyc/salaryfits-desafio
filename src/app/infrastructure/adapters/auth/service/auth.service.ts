a573053ff78aea6be063496938f430c0fa39b478
Carlos Bilby <carlos.bilby@gmail.com>
2024-09-08 21:37:16 -0300

    feat: - implementa jwt service no fluxo de autenticacao - configura o jwt module

--- src/app/infrastructure/adapters/auth/service/auth.service.ts
+++ src/app/infrastructure/adapters/auth/service/auth.service.ts
@@ -1,11 +1,13 @@
-import { Inject, Injectable, UnauthorizedException } from "@nestjs/common";
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from '../../../../domain/conta/dtos/login.dto';
 import { IContaRepository } from '../../../../domain/conta/repositories/iconta.repository';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
-    @Inject(IContaRepository) private contaRepository: IContaRepository
+    @Inject(IContaRepository) private contaRepository: IContaRepository,
+    private jwtService: JwtService,
   ) {}
 
   async logar(payload: LoginDto): Promise<any> {
@@ -18,7 +20,8 @@ export class AuthService {
       throw new UnauthorizedException();
     }
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { senha, ...result } = conta;
-    return result;
+    return {
+      acess_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -3,9 +3,18 @@ import { RepositoriesModule } from '../infrastructure/repositories/repositories.
 import { ContaService } from './conta.service';
 import { TransacaoService } from './transacao.service';
 import { AuthService } from '../infrastructure/adapters/auth/service/auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from '../infrastructure/adapters/auth/constants';
 
 @Module({
-  imports: [RepositoriesModule],
+  imports: [
+    RepositoriesModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '180s' },
+    }),
+  ],
   providers: [ContaService, TransacaoService, AuthService],
   exports: [ContaService, TransacaoService, AuthService],
 })
