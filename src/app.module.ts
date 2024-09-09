e9d269a15a3973db0997034903c9c87247def402
Carlos Bilby <carlos.bilby@gmail.com>
2024-09-09 02:25:33 -0300

    refactor: modifica gerenciamento de modules de controllers para uma unica classe

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,11 @@ import { AppService } from './app/services/app.service';
 import { ContaController } from './app/infrastructure/controllers/conta/conta.controller';
 import { ServicesModule } from './app/services/services.module';
 import { TransacaoController } from './app/infrastructure/controllers/transacao/transacao.controller';
-import { AuthController } from './app/infrastructure/adapters/auth/controller/auth.controller';
+import { AuthController } from './app/infrastructure/adapters/authentication/controller/auth.controller';
+import { ControllersModule } from './app/infrastructure/controllers/controllers.module';
 
 @Module({
-  imports: [ServicesModule],
-  controllers: [
-    AppController,
-    ContaController,
-    TransacaoController,
-    AuthController,
-  ],
+  imports: [ServicesModule, ControllersModule],
   providers: [AppService],
 })
 export class AppModule {}
