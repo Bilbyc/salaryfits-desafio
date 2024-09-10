8c546dc265914e46c3900ea925b4e24d48768c30
Carlos Bilby <carlos.bilby@gmail.com>
2024-09-10 03:47:35 -0300

    refactor: - renomeia CreateDepositoDto para nome generico afim de ser usado em operacoes de deposito e saque

--- src/app/domain/transacao/dtos/create-movimentacao.dto.ts
+++ src/app/domain/transacao/dtos/create-movimentacao.dto.ts
@@ -1,6 +1,6 @@
 import { IsNotEmpty, IsNumber } from 'class-validator';
 
-export class CreateDepositoDto {
+export class CreateMovimentacaoDto {
   @IsNotEmpty()
   @IsNumber()
   readonly valor: number;
